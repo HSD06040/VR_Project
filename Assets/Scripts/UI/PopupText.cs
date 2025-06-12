48c8f43fb75009c112ebf2eeb6d4ffaf98771f5e
HSD06040 <tinto0604@gmail.com>
2025-06-12 16:57:13 +0900

    [Feat] PopupUI

---
+++ Assets/Scripts/UI/PopupText.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class PopupText : MonoBehaviour
+{
+    [SerializeField] private TMP_Text popupText;
+    [SerializeField] private Button yesButton;
+    [SerializeField] private Button noButton;
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    public void SetupPopUp(string text, UnityAction yes, UnityAction no = null)
+    {
+        anim.SetTrigger("In");
+
+        popupText.text = text;
+        yesButton.onClick.AddListener(yes);
+
+        if(no != null )
+        {
+            noButton.onClick.AddListener(no);
+            noButton.onClick.AddListener(ClosePopup);
+        }
+        else
+            noButton.onClick.AddListener(ClosePopup);
+    }
+
+    private void OnDisable()
+    {
+        yesButton.onClick.RemoveAllListeners();
+        noButton.onClick.RemoveAllListeners();
+    }
+
+    private void ClosePopup()
+    {
+        anim.SetTrigger("Out");
+    }
+}
