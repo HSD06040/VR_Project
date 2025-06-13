927ffed85184a39853e5155b496f10a7184bc23a
HSD06040 <tinto0604@gmail.com>
2025-06-13 17:12:37 +0900

    [Fix] 핸드 트레킹 버그수정

---
+++ Assets/Scripts/HandGestureController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Hands;
+using UnityEngine.XR;
+
+public class HandGestureController : MonoBehaviour
+{
+    public GameObject targetObject;
+    public Transform handAttachPoint;
+    private bool isAttached = false;
+
+    private void Update()
+    {
+        if(isAttached)
+        {
+            targetObject.transform.position = handAttachPoint.position;
+            targetObject.transform.rotation = handAttachPoint.rotation;
+        }
+    }
+
+    public void OnGrabGesture()
+    {        
+        targetObject.transform.SetParent(handAttachPoint);
+        targetObject.SetActive(true);
+        isAttached = true;
+    }
+
+    public void OnReleaseGesture()
+    {
+        if (!isAttached) return;
+
+        targetObject.transform.SetParent(null);
+        targetObject.SetActive(false);
+        isAttached = false;
+    }
+}
